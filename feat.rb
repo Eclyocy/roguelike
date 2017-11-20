353f168a36bfd97b543621b6e38fd6ec9a9ef81e
Eclyocy <ksanph@gmail.com>
2017-11-20 17:35:43 +0300

    Added Feat
    Sketch with generator

---
+++ feat.rb
@@ -0,0 +1,39 @@
+require 'rexml/document'
+
+class Feat
+  attr_reader :name,
+              :description
+
+  alias :desc :description
+
+  def initialize(args)
+    @name = args[:name]
+    @description = args[:desc]
+  end
+
+  def to_s
+    "Feat:\n" +
+        "\tName: #{@name}\n" +
+        "\tDesc: #{@description}"
+  end
+
+  def self.generate
+    feats = []
+    REXML::Document.new(File.new('./rules.xml')).elements.each('rules/feats/feat') do |feat_xml|
+      description =
+          if feat_xml.elements['description']
+            feat_xml.elements['description'].text
+          else
+            nil
+          end
+      feats.push Feat.new({
+                              name: feat_xml.elements['name'].text,
+                              description: description
+                          })
+    end
+    feats.each{|feat| puts feat}
+    feats
+  end
+
+  Feat.generate
+end
